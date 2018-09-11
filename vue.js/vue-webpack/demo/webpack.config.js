489f837adbdab46aae5282737186b4dbc5d3f01c
731347546@qq.com <sf6308sf6308>
2018-09-11 15:50:42 +0800

    css nad style-loader install

--- vue.js/vue-webpack/demo/webpack.config.js
+++ vue.js/vue-webpack/demo/webpack.config.js
@@ -1,5 +1,25 @@
-var config = {
+var path = require('path');
 
-};
+var config = {
+    entry: {
+        main: './main'
+    },
+    output: {
+        path: path.join(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ] 
+    }
+}
 
 module.exports = config;
